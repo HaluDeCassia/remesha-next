854f131cfdb31a22d99b0b0ace52fc6c28f9d415
Halu de Cassia <haluanedecassia@gmail.com>
2021-02-25 18:57:51 -0300

    feat: add dinamic level in profile component

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,12 +1,15 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext'
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext)
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/haludecassia.png" />
       <div>
         <strong>Halú de Cássia</strong>
-        <p><img src="icons/level.svg"  alt="level"/> Level 1</p>
+        <p><img src="icons/level.svg"  alt="level"/> Level {level}</p>
       </div>
     </div>
   )
