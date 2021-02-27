8084083ec26d8f51abb48ae973c71f3678c3febd
Halu de Cassia <haluanedecassia@gmail.com>
2021-02-27 00:29:36 -0300

    feat: apply levelUp modal in challenges context

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,6 +1,7 @@
 import { createContext, useState, ReactNode, useEffect } from 'react'
 import Cookies from 'js-cookie'
 import challenges from '../../challenges.json'
+import { LevelUpModal } from '../components/LevelUpModal';
 
 interface ChallengesProviderProps {
   children: ReactNode
@@ -21,6 +22,7 @@ interface ChallengesContextData {
   currentExperience: number
   experienceToNextLevel: number
   level: number
+  closeLevelUpModal: () => void
   completeChallenge: () => void
   levelUp: () => void
   startNewChallenge: () => void
@@ -34,10 +36,16 @@ export const ChallengesProvider = ({ children, ...rest }: ChallengesProviderProp
   const [currentExperience, setCurrentExperience] = useState(rest.currentExperience | 0);
   const [challengesCompleted, setChallengesCompleted] = useState(rest.challengesCompleted | 0);
   const [activeChallenge, setActiveChallenge] = useState(null);
+  const [isLevelUpModalOpen, setIsLevelUpModalOpen] = useState(false);
 
   const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
 
-  const levelUp = () => setLevel(level + 1)
+  const levelUp = () => {
+    setLevel(level + 1)
+    setIsLevelUpModalOpen(true)
+  }
+
+  const closeLevelUpModal = () => setIsLevelUpModalOpen(false)
 
   const startNewChallenge = () => {
     const randomChallengeIndex = Math.floor(Math.random() * challenges.length)
@@ -93,6 +101,7 @@ export const ChallengesProvider = ({ children, ...rest }: ChallengesProviderProp
         currentExperience,
         experienceToNextLevel,
         level, 
+        closeLevelUpModal,
         completeChallenge,
         levelUp,
         startNewChallenge,
@@ -100,6 +109,7 @@ export const ChallengesProvider = ({ children, ...rest }: ChallengesProviderProp
       }}
     >
       {children}
+      {isLevelUpModalOpen && <LevelUpModal />}
     </ChallengesContext.Provider>
   )
 }
